fe8e343e755235299efe57268342bb315b39e7d2
Jagepard <dankorot@gmail.com>
2017-04-07 15:15:05 +0300

    Добавляет файл автозагрузки

---
+++ src/tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types = 1);
+
+define('APP_URL', 'http://example.com');
+
+require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/IRedirect.php';
+require_once dirname(__DIR__) . '/Redirect.php';
