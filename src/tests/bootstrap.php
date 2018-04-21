82add3557585ef0386bd104b224a18e06929448d
Jagepard <dankorot@gmail.com>
2018-04-21 14:23:38 +0300

    Убрал лишнее

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 
-declare(strict_types = 1);
-
 define('APP_URL', 'http://example.com');
 
 require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
-require_once dirname(__DIR__) . '/RedirectInterface.php';
-require_once dirname(__DIR__) . '/Redirect.php';
